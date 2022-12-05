27295b2efc340cb171b82ab459ee0471223aafdc
railek555 <railek555@gmail.com>
2022-12-05 21:16:40 +0300

    added html in posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,28 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+const path = require('path');
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { data } = await graphql(`
+  query Posts {
+  allMarkdownRemark {
+    nodes {
+      frontmatter {
+      category
+        url
+      }
+    }
+  }
+}`)
+  console.log(data)
+  data.allMarkdownRemark.nodes.forEach(node => {
+    const {url, category} = node.frontmatter;
+    actions.createPage({
+      path: `/${category}/${url}`,
+      component: path.resolve('./src/templates/single-post.js'),
+      context: {url}
+    })
   })
 }
+
+
